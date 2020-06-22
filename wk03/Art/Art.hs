5687a67741c9538f5d24f4c2c86fdee35cb549ea
Zhichao He <z5282955@student.unsw.edu.cn>
2020-06-22 17:19:08 +1000

    edit Art.hs

--- wk03/Art/Art.hs
+++ wk03/Art/Art.hs
@@ -4,15 +4,24 @@ import ShapeGraphics
 import Codec.Picture
 
 art :: Picture
-art = tree 10 (Point 400 800) (Vector 0 (-100)) red
+art = tree 8 10 (-1) 0.8 (Point 400 800) (Vector 0 (-100)) red 2 green
 
-tree :: Int -> Point -> Vector -> Colour -> Picture
-tree depth base direction colour
-  | depth == 0 = drawLine line
+tree :: Int -> Float -> Float -> Float -> Point -> Vector 
+        -> Colour -> Float -> Colour -> Picture
+tree depth -- depth of the tree
+     density  -- density of the tree, the larger the denser
+     lean -- leaning style, negative for left, positive for right
+     growth -- length relation between father and child, 0.8 means child is 0.8 length of father
+     base direction colour
+     leafRadius -- radius of leaves on the edges of the tree, 0 means no leaf
+     leafColour -- colour of the leaves
+  | depth == 0 = drawLine line ++ leaves
   | otherwise
     =  drawLine line
-    ++ tree (depth - 1) nextBase left nextColour -- left tree
-    ++ tree (depth - 1) nextBase right nextColour -- left tree
+    ++ tree (depth - 1) density lean growth nextBase left 
+       nextColour leafRadius leafColour -- left tree
+    ++ tree (depth - 1) density lean growth nextBase right 
+       nextColour leafRadius leafColour -- right tree
   where
     drawLine :: Line -> Picture
     drawLine (Line start end) =
@@ -21,11 +30,13 @@ tree depth base direction colour
     line = Line base nextBase
     nextBase = offset direction base
 
-    left = rotate (-pi /12) $ scale 0.8 $ direction
-    right = rotate (pi /12) $ scale 0.8 $ direction
+    left = rotate ((-pi + lean) / density) $ scale growth $ direction
+    right = rotate ((pi + lean) / density) $ scale growth $ direction
 
     nextColour =
       colour { redC = (redC colour) - 24, blueC = (blueC colour) + 24 }
+
+    leaves = [Circle nextBase leafRadius leafColour Solid SolidFill]
 
 -- Offset a point by a vector
 offset :: Vector -> Point -> Point
